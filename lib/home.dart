5e7e10f7ba3c8175599920984b0b615ebf5cc5dc
vanessa Enangue <vanessapenda22@gmail.com>
2022-06-26 17:46:57 +0100

    Correction de la récupération des menus à partir du fichier data.json dans assets/

--- lib/home.dart
+++ lib/home.dart
@@ -1,11 +1,10 @@
 import 'dart:convert';
+import 'dart:developer';
 import 'connexion.dart';
-
 
 import 'package:flutter/material.dart';
 
-
-var bannerItems = ["bouillon","ndole","riz","poulet","okok","fufu"];
+var bannerItems = ["bouillon", "ndole", "riz", "poulet", "okok", "fufu"];
 var bannerImage = [
   "images/bouillon.png",
   "images/ndole.png",
@@ -22,67 +21,90 @@ class Home extends StatelessWidget {
     var screenHeight = MediaQuery.of(context).size.height;
     var screenWidth = MediaQuery.of(context).size.width;
 
-    Future<List<Widget>> createList() async{
+    Future<List<Widget>> createList() async {
       List<Widget> items = <Widget>[];
-      String dataString = await DefaultAssetBundle.of(context).loadString("asset/data.json");
+      String dataString =
+          await DefaultAssetBundle.of(context).loadString("assets/data.json");
       List<dynamic> dataJSON = jsonDecode(dataString);
+      log("Data form json file");
+      log(dataString);
 
-      String finalString = "";
-      var object;
-      List<dynamic> dataList = object["placeItems"];
-      dataList.forEach((item) {
-        finalString = finalString + item + " | ";
-      });
+      dataJSON.forEach((object) {
+        String finalString = "";
+        List<dynamic> dataList = object["placeItems"];
+        dataList.forEach((item) {
+          finalString = finalString + item + " | ";
+        });
 
-      dataJSON.forEach((element) {
-        items.add(Padding(padding:const EdgeInsets.all(2.0),
+        items.add(Padding(
+          padding: const EdgeInsets.all(2.0),
           child: Container(
-            decoration:const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.all(Radius.circular(10.0)),
-              boxShadow:[
-                BoxShadow(
-                color: Colors.orange,
-                spreadRadius: 2.0,
-                blurRadius: 5.0
-              ),
-            ],
-            ) ,
+            decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black12,
+                      spreadRadius: 2.0,
+                      blurRadius: 5.0),
+                ]),
+            margin: const EdgeInsets.all(5.0),
             child: Row(
               mainAxisSize: MainAxisSize.max,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 ClipRRect(
-                  borderRadius:const BorderRadius.only(topLeft: Radius.circular(10.0), bottomLeft: Radius.circular(10.0)),
-                  child: Image.asset(object["placeImage"], height: 80,fit: BoxFit.cover,),
-
+                  borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(10.0),
+                      bottomLeft: Radius.circular(10.0)),
+                  child: Image.asset(
+                    object["placeImage"],
+                    width: 80,
+                    height: 80,
+                    fit: BoxFit.cover,
+                  ),
                 ),
-                Padding(padding:const EdgeInsets.all(8.0),
-                  child:  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(object["placeName"]),
-                      Padding(padding:const EdgeInsets.only(top: 2.0, bottom: 2.0),
-                        child: Text(finalString, style:const TextStyle(fontSize: 12.0, color: Colors.black54,), maxLines: 1,),
-                      ),
-                      Text("Min. Order:${object["minOder"]}", style:const TextStyle(fontSize: 12.0, color: Colors.black54),),
-
-                    ],
-                  )
+                SizedBox(
+                  width: 250,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(object["placeName"]),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 2.0, bottom: 2.0),
+                          child: Text(
+                            finalString,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                              fontSize: 12.0,
+                              color: Colors.black54,
+                            ),
+                            maxLines: 1,
+                          ),
+                        ),
+                        Text(
+                          "Min. Commandes: ${object["minOrder"]}",
+                          style:
+                              const TextStyle(fontSize: 12.0, color: Colors.black54),
+                        )
+                      ],
+                    ),
+                  ),
                 )
-
               ],
             ),
           ),
-        )
-        );
+        ));
       });
+
       return items;
     }
+
     return Scaffold(
         appBar: AppBar(
-
-          title:const Text("DORA FOOD"),
+          title: const Text("DORA FOOD"),
         ),
         body: Container(
           height: screenHeight,
@@ -91,107 +113,114 @@ class Home extends StatelessWidget {
             child: SingleChildScrollView(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget> [
+                children: <Widget>[
                   Padding(
-                    padding: const EdgeInsets.fromLTRB(10,5,10,5),
+                    padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget> [
-                        IconButton(icon:const Icon(Icons.menu), onPressed: (){}),
-                         const Text("DORA FOODS", style: TextStyle(fontSize: 25),),
-                        IconButton(icon:const Icon(Icons.person), onPressed: (){}),
+                      children: <Widget>[
+                        IconButton(
+                            icon: const Icon(Icons.menu), onPressed: () {}),
+                        const Text(
+                          "DORA FOODS",
+                          style: TextStyle(fontSize: 25),
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.person),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (BuildContext context) {
+                                return Connexion();
+                              }),
+                            );
+                          },
+                        ),
                       ],
                     ),
                   ),
                   BannerWidgetArea(),
-
                   Container(
-                    child: FutureBuilder(
-                      initialData:const <Widget> [Text("")],
-                      future: createList(),
-                      builder: (context,snapshot){
-                        if(snapshot.hasData){
-                          return  Padding(padding:const EdgeInsets.all(8.0),
-                            child: ListView(
-                              primary: false,
-                              shrinkWrap: true,
-                              // children: snapshot.data,
-                            ),
-                          );
-                        } else{
-                          return const CircularProgressIndicator();
-                        }
-                      }
-
-                    )
-                  ),
+                      child: FutureBuilder(
+                          initialData: const <Widget>[Text("")],
+                          future: createList(),
+                          builder: (context, snapshot) {
+                            if (snapshot.hasData) {
+                              return Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: ListView(
+                                  primary: false,
+                                  shrinkWrap: true,
+                                  children: snapshot.data as List<Widget>,
+                                ),
+                              );
+                            } else {
+                              return const CircularProgressIndicator();
+                            }
+                          })),
                 ],
               ),
             ),
           ),
-
-        )
-    );
+        ));
   }
 }
 
-
-
-class BannerWidgetArea extends StatelessWidget{
-
-
+class BannerWidgetArea extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var screenWidth = MediaQuery.of(context).size.width;
-    PageController controller = PageController(viewportFraction: 0.8, initialPage: 1);
+    PageController controller =
+        PageController(viewportFraction: 0.8, initialPage: 1);
     List<Widget> banners = <Widget>[];
-    for(int x=0; x<bannerItems.length;x++ ){
-      var bannerView = Padding(padding: const EdgeInsets.all(10.0),
+    for (int x = 0; x < bannerItems.length; x++) {
+      var bannerView = Padding(
+        padding: const EdgeInsets.all(10.0),
         child: Container(
           child: Stack(
             fit: StackFit.expand,
             children: <Widget>[
               Container(
-                decoration: const  BoxDecoration(
+                decoration: const BoxDecoration(
                     borderRadius: BorderRadius.all(Radius.circular(20.0)),
                     boxShadow: [
                       BoxShadow(
                           color: Colors.black54,
-                          offset: Offset(2.0,2.0),
+                          offset: Offset(2.0, 2.0),
                           blurRadius: 5.0,
-                          spreadRadius: 1.0
-                      )
-                    ]
-                ),
+                          spreadRadius: 1.0)
+                    ]),
               ),
-
               ClipRRect(
                   borderRadius: const BorderRadius.all(Radius.circular(20.0)),
-                  child: Image.asset(bannerImage[x],fit: BoxFit.cover,)
-              ),
-
+                  child: Image.asset(
+                    bannerImage[x],
+                    fit: BoxFit.cover,
+                  )),
               Container(
                 decoration: const BoxDecoration(
                   borderRadius: BorderRadius.all(Radius.circular(20.0)),
                   gradient: LinearGradient(
                       begin: Alignment.topCenter,
                       end: Alignment.bottomCenter,
-                      colors: [Colors.transparent, Colors.black]
-                  ),
+                      colors: [Colors.transparent, Colors.black]),
                 ),
               ),
-
               Padding(
                 padding: const EdgeInsets.all(10.0),
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end ,
+                  mainAxisAlignment: MainAxisAlignment.end,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
                       bannerItems[x],
-                      style: const TextStyle(fontSize: 12.0,color: Colors.white),
+                      style:
+                          const TextStyle(fontSize: 12.0, color: Colors.white),
                     ),
-                    const Text("merci beaucoup", style: TextStyle(fontSize: 12.0,color: Colors.white),
+                    const Text(
+                      "merci beaucoup",
+                      style: TextStyle(fontSize: 12.0, color: Colors.white),
                     ),
                   ],
                 ),
@@ -205,7 +234,7 @@ class BannerWidgetArea extends StatelessWidget{
 
     return Container(
       width: screenWidth,
-      height: screenWidth*9/16,
+      height: screenWidth * 9 / 16,
       child: PageView(
         controller: controller,
         scrollDirection: Axis.horizontal,
@@ -214,6 +243,3 @@ class BannerWidgetArea extends StatelessWidget{
     );
   }
 }
-
-
-
